8bbfc266c8e6e04eb7ac91cd21c768a965f293c9
rhinoxD <coolshiv0721@gmail.com>
2022-02-25 11:58:02 +0530

    POST Launches - Creating Launches 2

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const launchesRouter = express.Router();
 
-const { httpGetAllLaunches } = require('./launches.controller');
+const {
+  httpGetAllLaunches,
+  httpAddNewLaunch,
+} = require('./launches.controller');
 
-launchesRouter.get('/launches', httpGetAllLaunches);
+launchesRouter.get('/', httpGetAllLaunches);
+launchesRouter.post('/', httpAddNewLaunch);
 
 module.exports = launchesRouter;
