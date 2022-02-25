50338d236b78cb63c2e211ecead959c891116804
rhinoxD <coolshiv0721@gmail.com>
2022-02-25 10:26:09 +0530

    GET Launches

---
+++ server/src/routes/launches/launches.router.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const launchesRouter = express.Router();
+
+const { getAllLaunches } = require('./launches.controller');
+
+launchesRouter.get('/launches', getAllLaunches);
+
+module.exports = launchesRouter;
