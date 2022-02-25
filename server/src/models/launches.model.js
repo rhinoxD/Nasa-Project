d82ef37bbb30ba4de67cade965a1a91cba313320
rhinoxD <coolshiv0721@gmail.com>
2022-02-25 11:21:03 +0530

    POST Launches Creating Launches 1

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,12 +1,14 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
   destination: 'Kepler-442 b',
-  customer: ['NASA'],
+  customers: ['NASA'],
   upcoming: true,
   success: true,
 };
@@ -17,4 +19,17 @@ function getAllLaunches() {
   return Array.from(launches.values());
 }
 
-module.exports = { getAllLaunches };
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ['NASA'],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
+module.exports = { getAllLaunches, addNewLaunch };
