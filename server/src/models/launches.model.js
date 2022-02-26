c9e76cac482e4bbe603fed465e112b936906d054
rhinoxD <coolshiv0721@gmail.com>
2022-02-26 10:11:09 +0530

    DELETE Launches - Aborting Launches 2

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -36,7 +36,12 @@ function addNewLaunch(launch) {
   );
 }
 
-function abortLaunchById(launchId) {}
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
 
 module.exports = {
   getAllLaunches,
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -2,6 +2,7 @@ const {
   getAllLaunches,
   addNewLaunch,
   existsLaunchWithId,
+  abortLaunchById,
 } = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
@@ -31,12 +32,13 @@ function httpAddNewLaunch(req, res) {
 }
 
 function httpAbortLaunch(req, res) {
-  const launchId = req.params.id;
+  const launchId = Number(req.params.id);
   if (!existsLaunchWithId(launchId)) {
     return res.status(404).json({
       error: 'Launch not found',
     });
   }
+  const aborted = abortLaunchById(launchId);
   return res.status(200).json(aborted);
 }
 
