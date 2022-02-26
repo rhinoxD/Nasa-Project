c0baad287235647a216f0ae70a8151f6818734ab
rhinoxD <coolshiv0721@gmail.com>
2022-02-26 11:21:08 +0530

    Testing API Endpoints with Supertest GET

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,7 +1,12 @@
+const request = require('supertest');
+const app = require('../../app');
+
 describe('Test GET /launches', () => {
-  test('It should response with 200 success', () => {
-    const response = 200;
-    expect(response).toBe(200);
+  test('It should response with 200 success', async () => {
+    const response = await request(app)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200);
   });
 });
 
