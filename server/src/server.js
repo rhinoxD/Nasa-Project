48f0e8af81f099ab17feb84def9b1e3e1d688fdc
rhinoxD <coolshiv0721@gmail.com>
2022-03-01 10:14:34 +0530

    Connecting to MongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -8,7 +10,19 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
