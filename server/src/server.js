4975646488908630429fa7e9e1ac2ca1bcd17e24
rhinoxD <coolshiv0721@gmail.com>
2022-03-03 11:49:23 +0530

    Updating Tests for Mongoose 1

--- server/src/server.js
+++ server/src/server.js
@@ -1,28 +1,15 @@
 const http = require('http');
-require('dotenv').config();
-const mongoose = require('mongoose');
 
 const app = require('./app');
-
 const { loadPlanetsData } = require('./models/planets.model');
+const mongoConnect = require('./services/mongo');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoConnect();
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
---
+++ server/src/services/mongo.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
+async function mongoConnect() {
+  await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}
+
+module.exports = mongoConnect;
